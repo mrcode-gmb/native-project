fbe5edff4ee68c25a675f28badcd944679c192c8
Ahmad Isah <ahmadisah6293@gmail.com>
2025-03-13 15:00:17 +0100

    mrcode

---
+++ .history/app/index_20250313141541.tsx
@@ -0,0 +1,38 @@
+import { Platform } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function HomeScreen() {
+  if (Platform.OS === "web") {
+    return (
+      <WebView/>
+      // <iframe
+      //   src="https://miyamiyadatasub.com.ng"
+      //   style={{
+      //     width: "100%",
+      //     height: "100vh",
+      //     border: "none",
+      //     overflow: "hidden", // Hides scrollbar
+      //     scrollbarWidth: "none", // Firefox
+      //     msOverflowStyle: "none", // IE/Edge
+      //   }}
+      // />
+    );
+  }
+
+  return (
+    <>
+    
+      <iframe
+        src="https://miyamiyadatasub.com.ng"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+          overflow: "hidden", // Hides scrollbar
+          scrollbarWidth: "none", // Firefox
+          msOverflowStyle: "none", // IE/Edge
+        }}
+      />
+    </>
+  );
+}
---
+++ .history/app/index_20250313141546.tsx
@@ -0,0 +1,37 @@
+import { Platform } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function HomeScreen() {
+  if (Platform.OS === "web") {
+    return (
+      <iframe
+        src="https://miyamiyadatasub.com.ng"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+          overflow: "hidden", // Hides scrollbar
+          scrollbarWidth: "none", // Firefox
+          msOverflowStyle: "none", // IE/Edge
+        }}
+      />
+    );
+  }
+
+  return (
+    <>
+    
+      <iframe
+        src="https://miyamiyadatasub.com.ng"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+          overflow: "hidden", // Hides scrollbar
+          scrollbarWidth: "none", // Firefox
+          msOverflowStyle: "none", // IE/Edge
+        }}
+      />
+    </>
+  );
+}
